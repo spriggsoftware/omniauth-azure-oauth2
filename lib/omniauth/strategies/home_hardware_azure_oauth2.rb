08f900bbef484d6f0a6ca6bfa11515e4a2b1290c
nradford01 <nradford0171@gmail.com>
2020-11-23 12:09:10 -0500

    Update home_hardware_azure_oauth2.rb
    remove hh

--- lib/omniauth/strategies/home_hardware_azure_oauth2.rb
+++ lib/omniauth/strategies/home_hardware_azure_oauth2.rb
@@ -2,7 +2,7 @@ module OmniAuth
   module Strategies
     class HomeHardwareAzureOauth2 < OmniAuth::Strategies::AzureOauth2
 
-      option :name, 'home_hardware_azure_oauth2'
+      option :name, 'azure_oauth2_client2'
   	end
   end
 end
